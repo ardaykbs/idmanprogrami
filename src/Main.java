a8804cec3dbe1243664fef299158b1f688cce52a
Arda Y. <ardaykbs22@gmail.com>
2025-05-07 00:02:59 +0300

    klasör eklendi.

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("idman programına hoşgeldiniz");
+        System.out.println("situp");
+        System.out.println("burpee");
+        System.out.println("squat");
+        System.out.println("pushup");
+
+        System.out.println("bir idman oluşturun");
+
+        idman idmanci1 = new idman(20, 50,30,40);
+        while(idman.idmanbittimi)
+
+
+
+
+    }
+}
